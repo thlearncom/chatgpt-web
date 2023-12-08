65929ae2e008c2b17bddc4ec746ed433ee96fdf7
thlearn <thlearn@163.com>
2023-12-08 19:04:10 +0800

    修改预设

--- src/store/modules/settings/helper.ts
+++ src/store/modules/settings/helper.ts
@@ -10,8 +10,8 @@ export interface SettingsState {
 
 export function defaultSetting(): SettingsState {
   return {
-    systemMessage: 'You are ChatGPT, a large language model trained by OpenAI. Follow the user\'s instructions carefully. Respond using markdown.',
-    temperature: 0.8,
+    systemMessage: 'You are ChatGPT, a large language model trained by OpenAI.',
+    temperature: 0.5,
     top_p: 1,
   }
 }
